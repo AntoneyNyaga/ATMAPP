2e9ea5a01963e081d41682d925008d5eaf971dad
Antoney Nyaga <33865579+AntoneyNyaga@users.noreply.github.com>
2022-06-27 16:29:27 +0300

    new commit

--- ATMAPP/UI/Utility.cs
+++ ATMAPP/UI/Utility.cs
@@ -1,9 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace ATMAPP.UI
 {
+    public static void PrintMessage(string msg, bool success = true)
+    {
+        if (success)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        Console.WriteLine(msg);
+        Console.ResetColor();
+        PressEnterToContinue();
+    }
+
     public static class Utility
     {
         public static string GetUserInput(string prompt)
